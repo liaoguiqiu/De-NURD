b19a1be174a5d9476144dde50c4eff3aff6dec2c
Guiqiu <guiqiu.liao@icube.unistra.fr>
2020-02-10 13:27:35 +0100

    data_ge without small window

---
+++ De_NURD/Dataset_generator.py
@@ -0,0 +1,67 @@
+import cv2
+import numpy as np
+import os
+from analy import MY_ANALYSIS
+from analy import Save_signal_enum
+from scipy import signal 
+import random
+from median_filter_special import myfilter
+from Correct_sequence_iteration import VIDEO_PEOCESS
+from cost_matrix import COSTMtrix
+class DATA_Generator(object):
+     def __init__(self):
+        self.original_root = "..\\..\\saved_original_for_generator\\"
+        self.data_pair1_root = "..\\..\\saved_pair1\\"
+        self.data_pair2_root = "..\\..\\saved_pair2\\"
+        self.data_mat_root = "..\\..\\saved_matrix\\"
+        self.H  = 1024
+        self.W = 780
+        # read the signals  just use the existing path
+        self.saved_stastics = MY_ANALYSIS()
+        self.path_DS =  self.saved_stastics.read_my_signal_results()
+     def generate(self):
+         #read one from the original
+            #random select one IMG frome the oringinal 
+        read_id = 0
+        Len_steam =5
+        steam=np.zeros((Len_steam,self.H,self.W)) # create video buffer
+        while (1):
+            OriginalpathDirlist = os.listdir(self.original_root)    # 
+            sample = random.sample(OriginalpathDirlist, 1)  # 
+            Sample_path = self.original_root +   sample[0]
+            original_IMG = cv2.imread(Sample_path)
+            original_IMG  =   cv2.cvtColor(original_IMG, cv2.COLOR_BGR2GRAY)
+            original_IMG = cv2.resize(original_IMG, (self.W,self.H), interpolation=cv2.INTER_AREA)
+
+            #read the path and Image number from the signal file
+            #get the Id of image which should be poibnt to
+            Image_ID = int( self.path_DS.signals[Save_signal_enum.image_iD.value, read_id])
+            #get the path
+            path  = self.path_DS.path_saving[read_id,:]
+            path =  signal.resample(path, self.W)#resample the path
+            # create the shifted image
+            Shifted_IMG   = VIDEO_PEOCESS.de_distortion(original_IMG,path,Image_ID,0)
+            # save all the result
+            cv2.imwrite(self.data_pair1_root  + str(Image_ID) +".jpg", original_IMG)
+            cv2.imwrite(self.data_pair2_root  + str(Image_ID) +".jpg", Shifted_IMG)
+            ## validation 
+            #steam[Len_steam-1,:,:]  = original_IMG  # un-correct 
+            #steam[Len_steam-2,:,:]  = Shifted_IMG  # correct 
+            #Costmatrix,shift_used = COSTMtrix.matrix_cal_corre_full_version_2GPU(steam,0) 
+            #Costmatrix  = myfilter.gauss_filter_s (Costmatrix) # smooth matrix
+            #show1 =  Costmatrix 
+            #for i in range ( len(path)):
+            #    show1[int(path[i]),i]=254
+            #cv2.imwrite(self.data_mat_root  + str(Image_ID) +".jpg", show1)
+
+
+
+            print ("[%s]   is processed. test point time is [%f] " % (read_id ,0.1))
+
+            read_id +=1
+
+
+
+generator   = DATA_Generator()
+generator.generate()
+
--- De_NURD/De_NURD.py
+++ De_NURD/De_NURD.py
@@ -1,10 +1,7 @@
 #operatedir_pic =  "..\\initialbackground\\"
 #operatedir_video =  "D:\\PhD\\trying\\OCT\\P-ID_Name_25092019160318VIDEO.avi"
-#operatedir_video =  "..\\..\\OCT\\P-ID_Name_25092019160318VIDEO.avi"
+operatedir_video =  "..\\..\\OCT\\P-ID_Name_25092019160318VIDEO.avi"
 #operatedir_video =  "..\\..\\OCT\\P-ID_Name_25092019164030.avi"
-#E:\PhD\trying\OCT\OCT aligment
-operatedir_video =  "..\\..\\OCT\\OCT aligment\\22JAN2020AUTO_02.avi"
-
 #operatedir_video =  "..\\..\\OCT\\P-ID_Name_25092019161813-7500rpm-G1_0.05_4_25_extracted.avi"
 savedir_matrix  = "..\\..\\saved_matrix\\"
 savedir_original  = "..\\..\\saved_original\\"
@@ -16,8 +13,8 @@ import math
 import numpy as np
 import os
 import random
-#from matplotlib.pyplot import *
-#from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.pyplot import *
+from mpl_toolkits.mplot3d import Axes3D
 from median_filter_special import  myfilter
 from cost_matrix import  COSTMtrix
 
@@ -28,11 +25,11 @@ except ImportError:
     import xml.etree.ElementTree as ET 
 import sys 
 #GPU acceleration
-#from numba import vectorize
-#from numba import jit
-#@jit
-#def multiply(a, b):
-#    return a * b /255.0
+from numba import vectorize
+from numba import jit
+@jit
+def multiply(a, b):
+    return a * b /255.0
 per_image_Rmean = []
 per_image_Gmean = []
 per_image_Bmean = [] 
@@ -45,14 +42,14 @@ cap = cv2.VideoCapture(operatedir_video)
 # Check if camera opened successfully
 if (cap.isOpened()== False): 
   print("Error opening video stream or file")
-#fig = figure() 
+fig = figure() 
 # Read until video is completed
 Len_steam =5
 ret, frame = cap.read()
 if ret == True:
     H,W,_ = frame.shape
-H_start = 0
-H_end = W
+H_start = 50
+H_end = int(H/2)
  
 steam=np.zeros((Len_steam,H_end-H_start,W))
 steam2=np.zeros((Len_steam,H_end-H_start,W))
@@ -86,7 +83,7 @@ while(cap.isOpened()):
 
 
     #cv2.imwrite(savedir_matrix  + str(save_sequence_num) +".jpg", Costmatrix)
-    cv2.imwrite(savedir_original  + str(save_sequence_num) +".jpg", crop_H_test)
+    cv2.imwrite(savedir_original  + str(save_sequence_num) +".jpg", frame)
     cv2.imwrite(savedir_filtered_OCT  + str(save_sequence_num) +".jpg", filter_img)
     save_sequence_num+=1
     print ("[%s]   is processed." % (save_sequence_num))
